35b82ed228185f599b8a674c7e243a958d34a75a
Sean Darcy <darcys22@gmail.com>
2017-02-11 17:42:49 +1100

    works needs a date

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function handleFiles(event) {
 		var workbook = XLSX.read(data,{type:'binary'});
 		window.rows = [];
 		window.columns = [];
-    var csv = Papa.parse(XLSX.utils.sheet_to_csv(workbook.Sheets[workbook.SheetNames[0]]));
+    window.csv = Papa.parse(XLSX.utils.sheet_to_csv(workbook.Sheets[workbook.SheetNames[0]]));
     var body = document.getElementById("mappertable");
     var tbl = document.createElement('table');
     tbl.className = 'table';
@@ -11,32 +11,36 @@ function handleFiles(event) {
     var thead = document.createElement('thead');
     var th = document.createElement('th');
     thead.appendChild(th);
-    for (i = 0; i < csv.data[0].length; i++) {
+    for (i = 0; i < window.csv.data[0].length; i++) {
       columns[i] = '';
       var th = document.createElement('th');
       th.className = 'dropdown';
       var button = document.createElement('button');
       button.type="button";
+      button.id="c" + i;
       button.className = 'btn btn-default btn-sm dropdown-toggle';
       button.setAttribute('data-toggle',"dropdown");
-      button.innerHTML = 'Map Column <span class="caret"></span>'
+      button.innerHTML = 'Ignore Column <span class="caret"></span>'
       var ul = document.createElement('ul');
       ul.className = 'dropdown-menu';
       ul.innerHTML = `
-                      <li><a href="#">Account Description</a></li>
-                      <li><a href="#">Account Number</a></li>
-                      <li><a href="#">Other</a></li>
+                      <li><a href="#" id="">Ignore Column</a></li>
+                      <li><a href="#" id="Account">Account Description</a></li>
+                      <li><a href="#" id="Account_Number">Account Number</a></li>
+                      <li><a href="#" >Other</a></li>
                       <li role="separator" class="divider"></li>
-                      <li><a href="#">Debits</a></li>
-                      <li><a href="#">Credits</a></li>
-                      <li><a href="#">Negative Credits</a></li>
-                      <li><a href="#">Debits & Credits</a></li>`
+                      <li><a href="#" id="debit">Debits</a></li>
+                      <li><a href="#" id="credit">Credits</a></li>
+                      <li><a href="#" id="negcredit">Negative Credits</a></li>
+                      <li><a href="#" id="value">Debits & Credits</a></li>`
+
+
       th.appendChild(button);
       th.appendChild(ul);
       thead.appendChild(th);
     }
     tbl.appendChild(thead);
-    for (i = 0; i < csv.data.length; i++) {
+    for (i = 0; i < window.csv.data.length; i++) {
       rows[i] = true;
       var tr = document.createElement('tr');
       var td = document.createElement('td');
@@ -47,9 +51,9 @@ function handleFiles(event) {
       input.addEventListener("click", function(){toggleRow(this.id)}, false);
       td.appendChild(input);
       tr.appendChild(td);
-      for (j = 0; j < csv.data[i].length; j++) {
+      for (j = 0; j < window.csv.data[i].length; j++) {
         var td = document.createElement('td');
-        td.appendChild(document.createTextNode(csv.data[i][j]));
+        td.appendChild(document.createTextNode(window.csv.data[i][j]));
         tr.appendChild(td);
       }
       tbdy.appendChild(tr);
@@ -57,12 +61,78 @@ function handleFiles(event) {
     tbl.appendChild(tbdy);
     body.appendChild(tbl);
     $("#panel").removeClass('hidden');
+    $('ul.dropdown-menu li a').click(function (e) {
+      var $div = $(this).parent().parent().parent(); 
+      var $btn = $div.find('button');
+      $btn.html($(this).text() + ' <span class="caret"></span>');
+      $div.removeClass('open'); 
+      addColumn($btn.attr('id'), this.id);
+      e.preventDefault();
+      return false;
+    }); 
+    var convertButton= document.getElementById("convert");
+    convertButton.classList.remove('disabled');
+    convertButton.classList.remove('disabled');
+    convertButton.addEventListener("click", function(){convert()}, false);
 	});
 }
 
 function toggleRow(path) {
   window.rows[path] = !window.rows[path];
-  //row = document.getElementById(path);
+}
+
+function addColumn(column, desc) {
+  window.columns[parseInt(column.slice(1))] = desc;
+}
+function convert() {
+  var balance = 0;
+  var filtered = window.csv.data.filter(function(x) {
+    return window.rows[window.csv.data.indexOf(x)]
+  });
+
+  var jsontb = {"Prot":"Journal","Txn":{"Payee":"Import TB:","Date":"2016-6-30","Postings":[]}}
+
+//{"Account":"Asset.Cash","Amt":{"Value":"1000","Cur":"AUD"}}
+
+  for (var row in filtered) {
+    var posting = {};
+    for (i = 0; i < filtered[row].length; i++) {
+      var value = 0;
+      console.log(window.columns[i]);
+      switch(window.columns[i]) {
+          case "":
+              break;
+          case "debit":
+              value += parseInt(filtered[row][i]); 
+              break;
+          case "credit":
+              value -= parseInt(filtered[row][i]);
+              break;
+          case "negcredit":
+              value += parseInt(filtered[row][i]);
+              break;
+          case "value":
+              value = parseInt(filtered[row][i]);
+              break;
+          default:
+              posting[window.columns[i]] = filtered[row][i];
+      }
+    }
+    balance += value;
+    posting["Amt"] = {"Value": value, "Cur":"AUD"}
+    jsontb["Txn"]["Postings"].push(posting)
+  }
+  $('#tbbalance-info').html("Balance: " + balance);
+
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(jsontb));
+  var dlAnchorElem = document.getElementById('downloadAnchorElem');
+  dlAnchorElem.setAttribute("href",     dataStr     );
+  dlAnchorElem.setAttribute("download", "trialBalance.json");
+  dlAnchorElem.click();
+
+
+  console.log(jsontb);
+  console.log(balance);
 }
 
 function loadBinaryFile(path, success) {
